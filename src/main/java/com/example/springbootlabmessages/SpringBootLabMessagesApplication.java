c7651459c30953ca73ee3ef657181caafef64b99
Robin Alfengård <robin.alfengard@hotmail.com>
2024-03-22 11:26:08 +0100

    Init commit

---
+++ src/main/java/com/example/springbootlabmessages/SpringBootLabMessagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootlabmessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabMessagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLabMessagesApplication.class, args);
+    }
+
+}
