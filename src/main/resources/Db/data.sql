a6492a92a776803243692ef56f1dee00b41fb333
DannyRyd <danielrydenberg@gmail.com>
2024-03-22 14:36:49 +0100

    add compose.yaml, data.sql

---
+++ src/main/resources/Db/data.sql
@@ -0,0 +1,4 @@
+insert into User (username, firstName, lastName, email, profilePicture) values
+('Knatte', 'test', 'test', 'test', 'test'),
+('Fnatte', 'test', 'test', 'test', 'test'),
+('Tjatte', 'test', 'test', 'test', 'test');
