75ed001e0b5a9592e33f4f186c35f06f6b12796a
DannyRyd <danielrydenberg@gmail.com>
2024-03-22 13:13:02 +0100

    add User entity

---
+++ src/main/java/User/User.java
@@ -0,0 +1,40 @@
+package User;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@Getter
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String profilePicture;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String firstName, String lastName, String email, String profilePicture) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.profilePicture = profilePicture;
+    }
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "user_id")
+    List<Message> vaccinationList = new ArrayList<>();
+}
