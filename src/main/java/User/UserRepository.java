9d5687be4d155bee2e0689c085169bd437a3350c
DannyRyd <danielrydenberg@gmail.com>
2024-03-22 13:31:48 +0100

    add UserRepository

---
+++ src/main/java/User/UserRepository.java
@@ -0,0 +1,10 @@
+package User;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface UserRepository extends ListCrudRepository<User, Long> {
+
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+}
