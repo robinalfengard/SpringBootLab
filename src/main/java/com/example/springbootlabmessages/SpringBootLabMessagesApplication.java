1062a03202d517b66476445bc425862809ca8e2d
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-03-25 10:28:25 +0100

    Add Html and css

--- src/main/java/com/example/springbootlabmessages/SpringBootLabMessagesApplication.java
+++ src/main/java/com/example/springbootlabmessages/SpringBootLabMessagesApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootLabMessagesApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/springbootlabmessages/web/WebController.java
+++ src/main/java/com/example/springbootlabmessages/web/WebController.java
@@ -1,8 +1,14 @@
 package com.example.springbootlabmessages.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
+    @GetMapping("/home")
+    String home() {
+        return "home";
+    }
+
 }
