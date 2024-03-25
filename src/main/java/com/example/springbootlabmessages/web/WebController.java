8b3d6eb64e01eb399914c6c9972e3ad5d8d31ea6
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-03-25 10:13:49 +0100

    Add WebController and user, password

---
+++ src/main/java/com/example/springbootlabmessages/web/WebController.java
@@ -0,0 +1,8 @@
+package com.example.springbootlabmessages.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+}
