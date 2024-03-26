db5c615a984a1acf3eebe6d13398eedb5b5c215d
Erika Signal <erika.signal@gmail.com>
2024-03-26 11:04:29 +0100

    Add base for POST method

--- src/main/java/com/example/springbootlabmessages/web/WebController.java
+++ src/main/java/com/example/springbootlabmessages/web/WebController.java
@@ -1,17 +1,25 @@
 package com.example.springbootlabmessages.web;
 
+import com.example.springbootlabmessages.Message.Message;
 import com.example.springbootlabmessages.Message.MessageService;
+import com.example.springbootlabmessages.User.User;
+import com.example.springbootlabmessages.User.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.net.URI;
+
+@RestController
 public class WebController {
     private final MessageService messageService;
+    private final UserService userService;
 
-    public WebController(MessageService messageService) {
+    public WebController(MessageService messageService, UserService userService) {
         this.messageService = messageService;
+        this.userService = userService;
     }
 
     @GetMapping("/createmessage")
@@ -25,6 +33,12 @@ public class WebController {
         model.addAttribute("listOfMessages", listOfMessages);
         return "messages";
     }
-        
 
+    @PostMapping("/createmessage")
+    public ResponseEntity<Void> createMessage (@RequestBody Message message){
+        messageService.save(message);
+        return ResponseEntity.status(HttpStatus.SEE_OTHER)
+                .location(URI.create("/createmessage"))
+                .build();
+    }
 }
