328ac228cf11f34c09b36bff0fee91b94b95105b
Robin Alfengård <robin.alfengard@hotmail.com>
2024-03-28 11:44:24 +0100

    post connected to user added

--- src/main/java/com/example/springbootlabmessages/User/UserService.java
+++ src/main/java/com/example/springbootlabmessages/User/UserService.java
@@ -4,4 +4,23 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findByEmail(Object email) {
+        return userRepository.findByEmail(email.toString());
+    }
+
+    public User findById(int id) {
+        System.out.println(id + " sent to service");
+        if(userRepository.findById(Long.valueOf(id)).get() != null){
+            return userRepository.findById((long) id).get();
+        }
+        else System.out.println("could not find user");
+        return null;
+    }
 }
