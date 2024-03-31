b750ebcb3a50f8d5db7e64cf9b66d3a01aecdafc
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-03-31 15:46:13 +0200

    Change firstName to name och delete lastname in User.java

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT  INTO user(id, first_name, last_name, username, email, profile_picture)
-VALUES (1, 'Robin', 'Rydenberg', 'Anders','RD@gmail.com', 'https://robohash.org/1')
-     , (2, 'Eriak', 'Andersson', 'Hanna','RDB@gmail.com', 'https://robohash00.org/1');
+INSERT  INTO user(id, name, username, email, profile_picture)
+VALUES (1, 'Robin Rydenberg', 'Anders','RD@gmail.com', 'https://robohash.org/1')
+     , (2, 'Eriak Andersson', 'Hanna','RDB@gmail.com', 'https://robohash00.org/1');
 
 INSERT INTO message(is_public, timestamp, user_id, text, title)
 VALUES (1,CURRENT_TIMESTAMP(), 1 ,'Our world is a breathtaking tapestry of wonders, from the majestic peaks of towering mountains to the tranquil shores of crystal-clear oceans. Each sunrise paints the sky with a symphony of colors, while lush forests whisper tales of ancient wisdom. Diverse cultures intertwine, weaving a rich tapestry of traditions and beliefs. In every corner, nature''s beauty beckons, reminding us of the precious gift of life on this magnificent planet.','The Beauty of Our World')
